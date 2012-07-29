ae50e1ac765693d1d59c80fe69c673b5101a35d4
Krofna <krofnica996@gmail.com>
2012-07-29 11:50:13 +0200

    [74] Pathfinding.hpp

--- server/Pathfinding.cpp
+++ server/Pathfinding.cpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-#include "Pathfinding.h"
+#include "Pathfinding.hpp"
 #include "Map.hpp"
 #include "../shared/Math.hpp"
 #include <queue>
