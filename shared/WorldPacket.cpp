1dbcc1e7d369eccd21aa25d3ea1d6a4408856519
Krofna <krofnica996@gmail.com>
2012-08-01 15:33:53 +0200

    [asio_2] Add new files

---
+++ shared/WorldPacket.cpp
@@ -0,0 +1,60 @@
+/*
+Warrior of Dreamworld, 2D Massivly Mutiplayer Online Role-playing Game
+Copyright (C) 2012  Mislav Blazevic
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "WorldPacket.hpp"
+#include "Opcodes.hpp"
+
+void WorldPacket::Clear()
+{
+    ByteBuffer.clear();
+    ReadPos = WritePos = 0;
+    Opcode = (uint16)MSG_NULL;
+}
+
+size_t WorldPacket::GetSize()
+{
+    return ByteBuffer.size();
+}
+
+uint16 WorldPacket::GetOpcode()
+{
+    return Opcode;
+}
+
+void WorldPacket::SetOpcode(uint16 Opcode)
+{
+    this->Opcode = Opcode;
+}
+
+WorldPacket& WorldPacket::operator >>(uint8 data)   { Append<uint8>(data);  return *this; }
+WorldPacket& WorldPacket::operator >>(uint16 data)  { Append<uint16>(data); return *this; }
+WorldPacket& WorldPacket::operator >>(uint32 data)  { Append<uint32>(data); return *this; }
+WorldPacket& WorldPacket::operator >>(uint64 data)  { Append<uint64>(data); return *this; }
+WorldPacket& WorldPacket::operator >>(int8 data)    { Append<int8>(data);   return *this; }
+WorldPacket& WorldPacket::operator >>(int16 data)   { Append<int16>(data);  return *this; }
+WorldPacket& WorldPacket::operator >>(int32 data)   { Append<int32>(data);  return *this; }
+WorldPacket& WorldPacket::operator >>(int64 data)   { Append<int64>(data);  return *this; }
+
+WorldPacket& WorldPacket::operator <<(uint8& data)  { data = Read<int8>();  return *this; }
+WorldPacket& WorldPacket::operator <<(uint16& data) { data = Read<int16>(); return *this; }
+WorldPacket& WorldPacket::operator <<(uint32& data) { data = Read<uint32>();return *this; }
+WorldPacket& WorldPacket::operator <<(uint64& data) { data = Read<uint64>();return *this; }
+WorldPacket& WorldPacket::operator <<(int8& data)   { data = Read<int8>();  return *this; }
+WorldPacket& WorldPacket::operator <<(int16& data)  { data = Read<int16>(); return *this; }
+WorldPacket& WorldPacket::operator <<(int32& data)  { data = Read<int32>(); return *this; }
+WorldPacket& WorldPacket::operator <<(int64& data)  { data = Read<int64>(); return *this; }
---
+++ shared/WorldPacket.hpp
@@ -0,0 +1,77 @@
+/*
+Warrior of Dreamworld, 2D Massivly Mutiplayer Online Role-playing Game
+Copyright (C) 2012  Mislav Blazevic
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#ifndef WORLD_PACKET_H
+#define WORLD_PACKET_H
+
+#include "Defines.hpp"
+#include <cassert>
+#include <vector>
+
+class WorldPacket
+{
+public:
+    void Clear();
+    size_t GetSize();
+    uint16 GetOpcode();
+    void SetOpcode(uint16 Opcode);
+
+    inline WorldPacket& operator >>(uint8 data);
+    inline WorldPacket& operator >>(uint16 data);
+    inline WorldPacket& operator >>(uint32 data);
+    inline WorldPacket& operator >>(uint64 data);
+    inline WorldPacket& operator >>(int8 data);
+    inline WorldPacket& operator >>(int16 data);
+    inline WorldPacket& operator >>(int32 data);
+    inline WorldPacket& operator >>(int64 data);
+
+    inline WorldPacket& operator <<(uint8& data);
+    inline WorldPacket& operator <<(uint16& data);
+    inline WorldPacket& operator <<(uint32& data);
+    inline WorldPacket& operator <<(uint64& data);
+    inline WorldPacket& operator <<(int8& data);
+    inline WorldPacket& operator <<(int16& data);
+    inline WorldPacket& operator <<(int32& data);
+    inline WorldPacket& operator <<(int64& data);
+
+
+private:
+    template<class T> void Append(T data);
+    template<class T> T Read() const;
+
+    size_t ReadPos, WritePos;
+    uint16 Opcode;
+
+    std::vector<char> ByteBuffer;
+};
+
+template<class T> void WorldPacket::Append(T data)
+{
+    ByteBuffer.resize(ByteBuffer.size() + sizeof(data));
+    std::memcpy((void*)&ByteBuffer[WritePos], &data,  sizeof(data));
+    WritePos += sizeof(data);
+}
+
+template<class T> T WorldPacket::Read() const
+{
+    assert(ReadPos + sizeof(T) <= ByteBuffer.size());
+    T data = *((T*)&ByteBuffer[ReadPos]);
+    return data;
+}
+
+#endif
