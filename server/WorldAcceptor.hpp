1e2839539fdcc236ef3864e396300ede2a1ca1c0
Krofna <krofnica996@gmail.com>
2012-08-02 14:56:29 +0200

    [asio_7] Add some files

---
+++ server/WorldAcceptor.hpp
@@ -0,0 +1,39 @@
+/*
+Warrior of Dreamworld, 2D Massivly Mutiplayer Online Role-playing Game
+Copyright (C) 2012  Mislav Blazevic
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#ifndef AUTH_SESSION_H
+#define AUTH_SESSION_H
+
+#include "../shared/Opcodes.hpp"
+
+class WorldAcceptor
+{
+public:
+    WorldAcceptor(boost::asio::io_service& io);
+    ~WorldAcceptor();
+
+    void Accept();
+
+private:
+    void HandleAccept(WorldSession* Session, const boost::system::error_code& error);
+
+    boost::asio::ip::tcp::acceptor Acceptor;
+    SocketPtr NewSocket;
+};
+
+#endif
