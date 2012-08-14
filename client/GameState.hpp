87c1ad54172673825c76bd230550d78d23dae641
Krofna <krofnica996@gmail.com>
2012-08-14 10:59:39 +0200

    [thread_4] Virtual dtor. Thanks ne555 ^^

--- client/GameState.hpp
+++ client/GameState.hpp
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 class GameState
 {
 public:
+    virtual ~GameState(){}
     virtual void HandleEvent(sf::Event Event) = 0;
     virtual void Draw() = 0;
 };
