f01876279a0094253c9b5034b90cd0c17ae8c874
Lahfa Ryan <adelryan98@yahoo.fr>
2012-08-18 16:25:02 +0300

    Forward declaration of WorldSession
    Permit compilation for me.

--- shared/Opcodes.hpp
+++ shared/Opcodes.hpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef OPCODES_H
 #define OPCODES_H
 
-#include "WorldSession.hpp"
+class WorldSession;
 
 #define MSG_COUNT 7
 
