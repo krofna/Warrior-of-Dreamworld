cf5d4dfd8acffe45fb2ac428c62559c0f0e387e9
Krofna <krofnica996@gmail.com>
2012-08-21 09:18:02 +0200

    [93] Fix OpcodeTable on MSVC

--- shared/Opcodes.hpp
+++ shared/Opcodes.hpp
@@ -19,6 +19,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef OPCODES_H
 #define OPCODES_H
 
+#include "../shared/config.hpp"
+
 #ifdef MSVC
     #include "WorldSession.hpp"
 #else
