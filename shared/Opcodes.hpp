e45a2d28382e1d818d1cb2c55d4be5dc19fd5e13
Krofna <krofnica996@gmail.com>
2012-08-21 09:32:21 +0200

    [94] TODO & fix case

--- shared/Opcodes.hpp
+++ shared/Opcodes.hpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef OPCODES_H
 #define OPCODES_H
 
-#include "../shared/config.hpp"
+#include "../shared/Config.hpp"
 
 #ifdef MSVC
     #include "WorldSession.hpp"
