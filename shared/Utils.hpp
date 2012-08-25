21a0d4658fc08ec5981794acff762e3881697ac8
=LAHFA Ryan <adelryan987@hotmail.fr>
2012-08-25 16:54:30 +0200

    Use now Boost.Format Fix problem with %llu Now, you can use any formating you want with Format.

--- shared/Utils.hpp
+++ shared/Utils.hpp
@@ -3,54 +3,27 @@
 
 #include <string>
 #include <sstream>
+#include <boost/format.hpp>
 
+inline std::string Format(boost::format formater)
+{
+    return formater.str();
+}
 inline std::string Format(std::string const& toFormat)
 {
     return toFormat;
 }
-// TODO: Need more work.
-template<typename Value, typename... Values>
-inline std::string Format(std::string toFormat, Value const& val, Values... values)
+
+template<typename T, typename... Values>
+inline std::string Format(boost::format formater, T const& val, Values... values)
 {
-    std::ostringstream QueryStr;
+    return Format(formater % val, std::forward<Values>(values)...);
+}
 
-    size_t placeholderPos = toFormat.find('%');
-    bool useValues = false, writeValues = true, itsEnd = false;
-
-    if (placeholderPos != std::string::npos)
-    {
-        if(toFormat[placeholderPos + 1] == '%')
-        {
-            toFormat.erase(toFormat.begin() + placeholderPos, toFormat.begin() + placeholderPos);
-            writeValues = false;
-        }
-        else
-        {
-            toFormat.erase(toFormat.begin() + placeholderPos, toFormat.begin() + placeholderPos + 1);
-        }
-
-        if ((placeholderPos + 1) >= toFormat.size())
-            itsEnd = true;
-        useValues = true;
-    }
-
-    if (useValues)
-    {
-        if (writeValues)
-        {
-            if (itsEnd)
-                QueryStr << toFormat.substr(0, placeholderPos) << val;
-            else
-                QueryStr << toFormat.substr(0, placeholderPos) << val << Format(toFormat.substr(placeholderPos+1), values...);
-        }
-        else
-            QueryStr << toFormat.substr(0, placeholderPos + 1) << Format(toFormat.substr(placeholderPos + 2), val, values...);
-    }
-
-    else
-        QueryStr << toFormat;
-
-    return QueryStr.str();
+template<typename T, typename... Values>
+inline std::string Format(std::string const& toFormat, T const& val, Values... values)
+{
+    return Format(boost::format(toFormat) % val, std::forward<Values>(values)...);
 }
 
 #endif // UTILS_HPP_INCLUDED
