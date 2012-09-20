6a974e24aa38cd675b3ff312653c0ea4b2043e7c
Mislav Blazevic <krofnica996@gmail.com>
2012-09-20 20:48:36 +0200

    Remove includes

--- server/Server/Database/ObjectMgr.cpp
+++ server/Server/Database/ObjectMgr.cpp
@@ -20,8 +20,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "Spell.hpp"
 #include "Database.hpp"
 #include "Player.hpp"
-#include <fstream>
-#include <stdexcept>
 
 ObjectMgr sObjectMgr;
 
