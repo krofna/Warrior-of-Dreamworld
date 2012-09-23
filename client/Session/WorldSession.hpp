54235bca1b298186af174527f241802e1df19096
=LAHFA Ryan <adelryan987@hotmail.fr>
2012-09-23 16:36:14 +0200

    Fix compilation for client

--- client/Session/WorldSession.hpp
+++ client/Session/WorldSession.hpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef WORLD_SESSION_H
 #define WORLD_SESSION_H
 
-#include "../shared/WorldPacket.hpp"
+#include "shared/WorldPacket.hpp"
 #include "World.hpp"
 #include "boost/asio.hpp"
 #include "boost/thread.hpp"
