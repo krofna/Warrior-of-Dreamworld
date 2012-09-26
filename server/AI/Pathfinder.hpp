b59f0c101e323a3b50d5163ab65076ec299fa9fa
Krofna <krofnica996@gmail.com>
2012-09-26 14:34:21 +0200

    Cosmetic fix

--- server/AI/Pathfinder.hpp
+++ server/AI/Pathfinder.hpp
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "shared/Defines.hpp"
 #include "shared/Vector2.hpp"
 
-struct Map;
+class Map;
 class WorldObject;
 class Unit;
 
